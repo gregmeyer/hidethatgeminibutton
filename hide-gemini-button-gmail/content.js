e13ef74888ca3f084a90041d901e8e46485eb0cb
Greg Meyer <greg@gregmeyer.com>
2025-01-16 18:12:40 -0800

    remove service worker
    remove service worker js, change div selection

--- hide-gemini-button-gmail/content.js
+++ hide-gemini-button-gmail/content.js
@@ -1,22 +1,10 @@
 function modifyDivClasses() {
     // Select all divs with the class 'einvLd'
-    const divs = document.querySelectorAll("div.einvLd");
+    const divs = document.querySelectorAll("div:has(> div[aria-label='Gemini'])");
+    
     divs.forEach(div => {
         div.style.display = "none"; // Hide the div
         console.log("Modified div:", div);
-    });
-}
-
-// Service Worker preload support
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch(error => {
-                console.error('Service Worker registration failed:', error);
-            });
     });
 }
 
